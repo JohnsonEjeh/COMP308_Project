a6c4d5ad8831f29862cedce7681fbc266983b65a
maljelma <133683358+maljelma@users.noreply.github.com>
2024-04-11 23:15:01 -0400

    updated graphQL responce to be data.status for {login, register}

--- client/user-app/src/queries/authQueries.js
+++ client/user-app/src/queries/authQueries.js
@@ -2,13 +2,13 @@ import { gql } from "@apollo/client";
 
 const LOGIN_MUTATION = gql`
   mutation Login($username: String!, $password: String!) {
-    signIn(username: $username, password: $password)
+    status:signIn(username: $username, password: $password)
   }
 `;
 
 const REGISTER_MUTATION = gql`
   mutation Register($username: String!, $password: String!) {
-    signUp(username: $username, password: $password)
+    status:signUp(username: $username, password: $password)
   }
 `;
 
