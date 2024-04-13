474ff93728f34b3f0b9c84009cc1bd4336779707
Ejeh <ejehsax@gmail.com>
2024-04-13 12:00:01 -0400

    fixed exposure

---
+++ server/config/env/production.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    db: "mongodb+srv://johnsonejeh02:DLKyZfyzWxMif4lk@cluster0.ox5nfnp.mongodb.net/308-project?retryWrites=true&w=majority",
+    sessionSecret: "developmentSessionSecret",
+    secretKey: "real_secret",
+  };
